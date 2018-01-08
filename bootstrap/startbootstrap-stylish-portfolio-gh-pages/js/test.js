f4b28e3e517067eb503b708fdd5dd4d2c16812fa
QQYamBall <s104213019@mail1.ncnu.edu.tw>
2018-01-09 01:41:40 +0800

    半夜的小更動

--- bootstrap/startbootstrap-stylish-portfolio-gh-pages/js/test.js
+++ bootstrap/startbootstrap-stylish-portfolio-gh-pages/js/test.js
@@ -2,34 +2,23 @@ function list_Photo(){
   console.log('good');
 		$.ajax({
 		  type: 'GET',
-<<<<<<< HEAD
       //dataType: "binary",
       url: "http://localhost/frog-master/add/show.php?act=frog",
       success : function(data){
-      console.log(data.length);
+      var count = Object.keys(data);
+      console.log(count);
       $('#showtable').html('');
-      for(i=0;i<data.length;i=i+1){
+      for(i=0;i<count;i=i+1){
       $('#showtable').prepend(
       `"<div class="col-md-7">
       <a data.id="${i}" herf="#" class="JM"><h2><img src="data[0].filepic"></h2></a>
       </div>"`
     )}
-=======
-      url: "http://localhost/frog-master/add/show.php?act=frog",
-      success : function(data){
-      console.log(data);
-      $('#about').prepend(
-        `"<div class="col-md-7">
-        <h2><img src="data[0].filepic"/></h2>
-        </div>"`
-      )
->>>>>>> f6dda5813574423118c9030fbe421b4a3c60ee36
     },error : function(err){
-      console.log(err.status);
+      console.log('err');
     },
   });
 }
-<<<<<<< HEAD
 $('#showtable').on("click",'.JM',function(){
 	var src = $(this).html();
 	$('#modalshow').html(src);
@@ -37,12 +26,9 @@ $('#showtable').on("click",'.JM',function(){
 	var id = $(this).prop("id");
   QQ(id);
 });
-=======
->>>>>>> f6dda5813574423118c9030fbe421b4a3c60ee36
 $('#frogbutton').on("click",function(){
     console.log("success!");
     list_Photo();
-
 });
 function List(){
   $('#about').html(
