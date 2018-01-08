95dfd823d865e59173361c1d21e895d36d2c52f6
DESKTOP-2SQEHMV\user <s104213037@mail1.ncnu.edu.tw>
2018-01-08 15:23:23 +0800

    debug

--- exif/readexif.php
+++ exif/readexif.php
@@ -1,25 +1,27 @@
 <?php
 require("dbconnect.php"); //匯入連結資料庫之共用程式碼
-$id=(int)$_REQUEST['id'];
-$sql = "select * from frogphotos where id=$id;"; //預設post進來的是id
+$id=(int) $_REQUEST['id'];
+$sql = "select * from frogphotos where id=$id;";
 $result=mysqli_query($conn,$sql) or die("DB Error: Cannot retrieve message."); //執行SQL查詢
-?>
-<?php
+
+while (	$row=mysqli_fetch_array($result)) {
+$sql2 = "select * from frogphotos where title=$row[2];";//row[2]是filepic
 $result2=mysqli_query($conn,$sql);
+$row2=mysqli_fetch_assoc($result2);
 //開啟圖片檔
-$file = fopen($_FILES["upfile"]["tmp_name"], "rb");
+$file = fopen($_FILES["$row2"]["tmp_name"], "rb");
 // 讀入圖片檔資料
-$fileContents = fread($file, filesize($_FILES["upfile"]["tmp_name"])); 
+$fileContents = fread($file, filesize($_FILES["$row2"]["tmp_name"])); 
 //關閉圖片檔
 fclose($file);
 // 圖片檔案資料編碼
   $fileContents = base64_encode($id);
   //連結SQL Server
-$dbnum=mssql_connect("photos","root","");
+$dbnum=mysql_connect("photos","root","");
 //選取資料庫
-mssql_select_db("photos");
+mysql_select_db("photos");
 //組合查詢字串
-        $SQLSTR="Insert into myimage (resolution,
+        $SQLSTR="Insert into exif (resolution,
 		camera,aperture,exposure,isoSpeed,focaLength,
 		saturation,whiteBalance,phototime) values(
 				'". $_FILES["upfile"]["resolution"] . "',
@@ -32,31 +34,32 @@ mssql_select_db("photos");
 				'". $_FILES["upfile"]["whiteBalance"] . "',
 				'". $_FILES["upfile"]["phototime"] . "',
 				  '". $fileContents . "')";
-?>
+
+}?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=big5">
 <title>圖片檔案上傳</title>
 </head><body><h3>圖檔存入相關資訊：<hr></h3>
 
-<?
+<?php
     
-	  echo "解析度：" . $_FILES["upfile"]["resolution"] . "<BR>";
-	  echo "相機型號：" . $_FILES["upfile"]["camera"] . "<BR>";
-	  echo "光圈孔徑：" . $_FILES["upfile"]["aperture"] . "<BR>";
-	  echo "曝光時間：" . $_FILES["upfile"]["exposure"] . "<BR>";
-	  echo "ISO速度：" . $_FILES["upfile"]["isoSpeed"] . "<BR>";
-	  echo "焦距：" . $_FILES["upfile"]["focaLength"] . "<BR>";
-	  echo "飽和度：" . $_FILES["upfile"]["saturation"] . "<BR>";
-      echo "白平衡：" . $_FILES["upfile"]["whiteBalance"] . "<BR>";
-	  echo "拍攝時間" . $_FILES["upfile"]["phototime"] . "<BR>";
+	  echo "解析度：" . $_FILES["$row2"]["resolution"] . "<BR>";
+	  echo "相機型號：" . $_FILES["$row2"]["camera"] . "<BR>";
+	  echo "光圈孔徑：" . $_FILES["$row2"]["aperture"] . "<BR>";
+	  echo "曝光時間：" . $_FILES["$row2"]["exposure"] . "<BR>";
+	  echo "ISO速度：" . $_FILES["$row2"]["isoSpeed"] . "<BR>";
+	  echo "焦距：" . $_FILES["$row2"]["focaLength"] . "<BR>";
+	  echo "飽和度：" . $_FILES["$row2"]["saturation"] . "<BR>";
+      echo "白平衡：" . $_FILES["$row2"]["whiteBalance"] . "<BR>";
+	  echo "拍攝時間:" . $_FILES["$row2"]["phototime"] . "<BR>";
 
-      if ( $_FILES["upfile"]["size"] > 0 ) 
+      if ( $_FILES["$row2"]["size"] > 0 ) 
         {
          //開啟圖片檔
-         $file = fopen($_FILES["upfile"]["tmp_name"], "rb");
+         $file = fopen($_FILES["$row2"]["tmp_name"], "rb");
          // 讀入圖片檔資料
-         $fileContents = fread($file, filesize($_FILES["upfile"]["tmp_name"])); 
+         $fileContents = fread($file, filesize($_FILES["$row2"]["tmp_name"])); 
          //關閉圖片檔
          fclose($file);
 
@@ -64,29 +67,29 @@ mssql_select_db("photos");
          $fileContents = base64_encode($id);
 
          //連結SQL Server
-         $dbnum=mssql_connect("photos","root","");
+         $dbnum=mysql_connect("photos","root","");
          //選取資料庫
-         mssql_select_db("photos");
+         mysql_select_db("photos");
          //組合查詢字串
-         $SQLSTR="Insert into myimage (resolution,
+         $SQLSTR="Insert into exif (resolution,
 		camera,aperture,exposure,isoSpeed,focaLength,
 		saturation,whiteBalance,phototime) values('"
-                  . $_FILES["upfile"]["resolution"] . "',
-				'". $_FILES["upfile"]["camera"] . "',
-				'". $_FILES["upfile"]["aperture"] . "',
-				'". $_FILES["upfile"]["exposure"] . "',
-				'". $_FILES["upfile"]["isoSpeed"] . "',
-				'". $_FILES["upfile"]["focaLength"] . "',
-				'". $_FILES["upfile"]["saturation"] . "',
-				'". $_FILES["upfile"]["whiteBalance"] . "',
-				'". $_FILES["upfile"]["phototime"] . "',
+                  . $_FILES["$row2"]["resolution"] . "',
+				'". $_FILES["$row2"]["camera"] . "',
+				'". $_FILES["$row2"]["aperture"] . "',
+				'". $_FILES["$row2"]["exposure"] . "',
+				'". $_FILES["$row2"]["isoSpeed"] . "',
+				'". $_FILES["$row2"]["focaLength"] . "',
+				'". $_FILES["$row2"]["saturation"] . "',
+				'". $_FILES["$row2"]["whiteBalance"] . "',
+				'". $_FILES["$row2"]["phototime"] . "',
 				  '". $fileContents . "')";
          //將圖片檔案資料寫入資料庫
-         if(!mssql_query($SQLSTR)==0)
+         if(!mysql_query($SQLSTR)==0)
            {
             echo "您所上傳的檔案已儲存進入資料庫<a href=\"readexif.php?filename="
-                 . $_FILES["upfile"]["name"] . "\">"
-                 . $_FILES["upfile"]["name"] . "</a>";
+                 . $_FILES["$row2"]["name"] . "\">"
+                 . $_FILES["$row2"]["name"] . "</a>";
            }
          else
            {
