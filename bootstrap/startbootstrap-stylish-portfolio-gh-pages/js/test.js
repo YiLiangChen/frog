de71c516aea34ba9dc5ef6e5751981da3790485c
QQYamBall <s104213019@mail1.ncnu.edu.tw>
2018-01-08 23:24:54 +0800

    一點小小更新

--- bootstrap/startbootstrap-stylish-portfolio-gh-pages/js/test.js
+++ bootstrap/startbootstrap-stylish-portfolio-gh-pages/js/test.js
@@ -2,23 +2,31 @@ function list_Photo(){
   console.log('good');
 		$.ajax({
 		  type: 'GET',
+      dataType: "binary",
       url: "http://localhost/frog-master/add/show.php?act=frog",
       success : function(data){
-      console.log(data);
-      $('#about').prepend(
+      console.log(data[0]);
+      $('#showtable').prepend(
         `"<div class="col-md-7">
-        <h2><img src="data[0].filepic"/></h2>
-        </div>"`
+      for(i=0;i<data.length;i++){
+      <a id="${data.id}" herf="#" class="JM"><h2><img src="data:image/jpeg;base64,{data[0].filepic}"/></h2></a>
+      }</div>"`
       )
     },error : function(err){
-      console.log(err.status);
+      console.log('err');
     },
   });
 }
+$('#showtable').on("click",'.JM',function(){
+	var src = $(this).html();
+	$('#modalshow').html(src);
+	console.log("安安");
+	var id = $(this).prop("id");
+	QQ(id);
+});
 $('#frogbutton').on("click",function(){
     console.log("success!");
     list_Photo();
-
 });
 function List(){
   $('#about').html(
