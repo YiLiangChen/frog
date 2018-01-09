17bdc133da629507461102212184154ab3ac83dc
Matchacoffee <a24556699@gmail.com>
2018-01-10 06:13:09 +0800

    修改 刪除 前端完成版

--- bootstrap/startbootstrap-stylish-portfolio-gh-pages/js/test.js
+++ bootstrap/startbootstrap-stylish-portfolio-gh-pages/js/test.js
@@ -1,21 +1,22 @@
 function list_Photo(){
-		$.ajax({
-		  type: 'GET',
+	$.ajax({
+	  type: 'GET',
       dataType :'json',
       url: "http://localhost/add/show.php?act=frog",
       success : function(data){
-        //data[0].filepic = data[0].filepic.replace(/%/g, "+");
-        //reader.readAsDataURL(data);
-        $('#showtable').html('');
-        for(i=0;i<data.length;i=i+1){
-          $('#showtable').prepend(`
-            <a data.id=${data[i].id} herf="#" name="${data[i].filename}" class="JM"><img width="300px" height="200px" src="data:image/jpeg;base64,${data[i].filepic}"></a>
-            `)
-        }
-    },error : function(err){
-      console.log('err');
-    },
-  });
+		show_Photo(data);
+	  },error : function(err){
+			console.log('err');
+	  },
+    });
+}
+function show_Photo(data){
+	$('#showtable').html('');
+	for(i=0;i<data.length;i=i+1){
+		$('#showtable').prepend(`
+        <a data.id=${data[i].id} herf="#" name="${data[i].filename}" class="JM"><img width="300px" height="200px" src="data:image/jpeg;base64,${data[i].filepic}"></a>
+        `);
+    }
 }
 function Post_id(name){
   $.ajax({
@@ -31,7 +32,7 @@ function Post_id(name){
         <div>camera = <input type="text" id="camera" placeholder="${data[0].camera}" > </div><br/>
         <div>aperture = <input type="text" id="aperture" placeholder="${data[0].aperture}" > </div><br/>
         <div>exposure = <input type="text" id="exposure" placeholder="${data[0].exposure}" > </div><br/>
-        <div>IsoSpeed = <input type="text" id="IsoSpeed" placeholder="${data[0].isoSpeed}" > </div><br/>
+        <div>isoSpeed = <input type="text" id="isoSpeed" placeholder="${data[0].isoSpeed}" > </div><br/>
         <div>focalLength = <input type="text" id="focalLength" placeholder="${data[0].focalLength}" > </div><br/>
         <div>saturation = <input type="text" id="saturation" placeholder="${data[0].saturation}" > </div><br/>
         <div>whiteBalance = <input type="text" id="whiteBalance" placeholder="${data[0].whiteBalance}" > </div><br/>
@@ -42,11 +43,12 @@ function Post_id(name){
 }
 $('#showtable').on("click",'.JM',function(){
 	var src = $(this).html();
+	$('#modalshow').html('');
 	$('#modalshow').append(src);
 	var id = $(this).prop("id");
-  var filename = $(this).prop("name");
-  Post_id(filename);
-  changepic(id,filename);
+	var filename = $(this).prop("name");
+	Post_id(filename);
+	changepic(id,filename);
 });
 $('#frogbutton').on("click",function(){
     list_Photo();
@@ -56,20 +58,11 @@ function List(){
     '"<img src="img class src="img/butterfly.jpg"">"'
   )
 };
-$('#frogicon').on("click",function(){
-  //console.log("good");
-  $('#showtable').html('');
-  for(i=0;i<4;i=i+1){
-  $('#showtable').prepend(
-    `<img src="data:image/jpeg;base64," width="150px" height="100px">&nbsp &nbsp`
-    );
-  }
-});
 function changepic(id,filename){
   console.log('change');
     $('#edit').on('click',function(){
-        ajax({
-            url: "http://localhost/add/edit.php?act=edit",
+        $.ajax({
+            url: "http://localhost/add/edit.php",
             type:"POST",
             data:{
             textName:$('#textName').val(),
@@ -78,28 +71,29 @@ function changepic(id,filename){
             camera:$('#camera').val(),
             aperture:$('#aperture').val(),
             exposure:$('#exposure').val(),
-            IsoSpeed:$('#IsoSpeed').val(),
-            focalLengh:$('#focalLength').val(),
+            isoSpeed:$('#isoSpeed').val(),
+            focalLength:$('#focalLength').val(),
             saturation:$('#saturation').val(),
             whiteBalance:$('#whiteBalance').val(),
             photoTime:$('#photoTime').val(),
-            id:id,
+            filename:filename,
             },
             success:function(data){
-                QQ(data);
+				aler('修改成功!');
             },
             error:function(error){
                 alert("QQ");
             }
         });
     });
-    $('delete').on('click',function(){
-        ajax({
-            url: "http://localhost/add/edit.php?act=delete",
+    $('#delet').on('click',function(){
+        $.ajax({
+            url: "http://localhost/add/delete.php?act=delete",
             type:"POST",
-            data:{id:id},
+            data:{filename:filename},
             success:function(data){
                 alert("ok");
+				show_Photo(data);
             },
             error:function(error){
                 alert("QQ");
