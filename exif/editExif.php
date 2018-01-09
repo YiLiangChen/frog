e32bd0c16391bb15200933428deeb7b8f136ae09
kukula87 <kyky159k@gmail.com>
2018-01-09 16:52:17 +0800

    修改 editExif.php

--- exif/editExif.php
+++ exif/editExif.php
@@ -1,52 +1,61 @@
 <?php
-require'DBconnect.php';
+require 'DBconnect.php';
 header('Access-Control-Allow-Origin:*');
 
 try{
-	$id = (int) $_REQUEST['id'];
-	/*$resolution=$_REQUEST['resolution'];	$camera=$_REQUEST['camera'];
-	$aperture=$_REQUEST['aperture'];	$exposure=$_REQUEST['exposure'];
-	$isoSpeed=$_REQUEST['isoSpeed'];	$focalLength=$_REQUEST['focalLength'];
-	$saturation=$_REQUEST['saturation'];	$whiteBalance=$_REQUEST['whiteBalance'];
-	$photoTime=$_REQUEST['photoTime'];  */
-	if (array_key_exists('resolution', $exif)) {
-      $resolution = $exif['resolution'];
-    }else
-	if (array_key_exists('camera', $exif)) {
-      $camera = $exif['camera'];
-    }else
-	if (array_key_exists('aperture', $exif)) {
-      $aperture = $exif['aperture'];
-    }else
-    if (array_key_exists('exposure', $exif)) {
-      $exposure = $exif['exposure'];
-    }else
-	if (array_key_exists('isoSpeed', $exif)) {
-      $isoSpeed = $exif['isoSpeed'];
-    }else	
-	if (array_key_exists('focalLength', $exif)) {
-      $focalLength = $exif['focalLength'];
-    }else
-	if (array_key_exists('saturation', $exif)) {
-      $saturation = $exif['saturation'];
-    }else
-	if (array_key_exists('whiteBalance', $exif)) {
-      $whiteBalance = $exif['whiteBalance'];
-    }else
-	if (array_key_exists('photoTime', $exif)) {
-      $photoTime = $exif['photoTime'];
-    }else	
+	$id = (int) $_POST['id'];
+	
+	if (isset($_POST['resolution'])) {
+    $resolution = $_POST['resolution'];
+  }else{
+		$resolution = "Unknown";
+	}
+	if (isset($_POST['camera'])) {
+    $camera = $_POST['camera'];
+  }else{
+		$camera = "Unknown";
+	}
+	if (isset($_POST['aperture'])) {
+    $aperture = $_POST['aperture'];
+  }else{
+		$aperture = "Unknown";
+	}
+  if (isset($_POST['exposure'])) {
+    $exposure = $_POST['exposure'];
+  }else{
+		$exposure = "Unknown";
+	}
+	if (isset($_POST['isoSpeed'])) {
+    $isoSpeed = $_POST['isoSpeed'];
+  }else{
+		$isoSpeedn = "Unknown";
+	}
+	if (isset($_POST['focalLength'])) {
+    $focalLength = $_POST['focalLength'];
+  }else{
+		$focalLength = "Unknown";
+	}
+	if (isset($_POST['saturation'])) {
+    $saturation = $_POST['saturation'];
+  }else{
+		$saturation = "Unknown";
+	}
+	if (isset($_POST['whiteBalance'])) {
+    $whiteBalance = $_POST['whiteBalance'];
+  }else{
+		$whiteBalance = "Unknown";
+	}
+	if (isset($_POST['photoTime'])) {
+    $photoTime = $_POST['photoTime'];
+  }else{
+		$photoTime = "Unknown";
+	}
+
 	$sql = "update exif set resolution='$resolution',camera='$camera',aperture='$aperture',exposure='$exposure',isoSpeed='$isoSpeed',
 		focalLength='$focalLength',saturation='$saturation',whiteBalance='$whiteBalance',photoTime='$photoTime' where id=$id;";
-		$pdo->exec($sql);
-		echo"修改完成嘞~";
-		
-        $sql2 = "SELECT resolution,camera,aperture,exposure,isoSpeed,focalLength,saturation,whiteBalance,photoTime,id FROM exif";
-        $stmt = $pdo->prepare($sql2);
-        $stmt->execute();
-        $result = $stmt->fetchall(PDO::FETCH_ASSOC);
-        return $result;
-    }catch (PDOException $e) {
-        echo "爆了，重打一次吧";
-  }
+	$pdo->exec($sql);
+	echo"修改完成嘞~";
+  }catch (PDOException $e) {
+    echo "爆了，重打一次吧";
+}
 ?>
