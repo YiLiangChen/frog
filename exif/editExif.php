5fe0f0f21b0a855da4e43341a59a374a9a057f8a
damayan <33472771+s104213037@users.noreply.github.com>
2018-01-09 15:11:58 +0800

    修改寫法

--- exif/editExif.php
+++ exif/editExif.php
@@ -4,15 +4,38 @@ header('Access-Control-Allow-Origin:*');
 
 try{
 	$id = (int) $_REQUEST['id'];
-	$resolution=$_REQUEST['resolution'];
-	$camera=$_REQUEST['camera'];
-	$aperture=$_REQUEST['aperture'];
-	$exposure=$_REQUEST['exposure'];
-	$isoSpeed=$_REQUEST['isoSpeed'];
-	$focalLength=$_REQUEST['focalLength'];
-	$saturation=$_REQUEST['saturation'];
-	$whiteBalance=$_REQUEST['whiteBalance'];
-	$photoTime=$_REQUEST['photoTime'];
+	/*$resolution=$_REQUEST['resolution'];	$camera=$_REQUEST['camera'];
+	$aperture=$_REQUEST['aperture'];	$exposure=$_REQUEST['exposure'];
+	$isoSpeed=$_REQUEST['isoSpeed'];	$focalLength=$_REQUEST['focalLength'];
+	$saturation=$_REQUEST['saturation'];	$whiteBalance=$_REQUEST['whiteBalance'];
+	$photoTime=$_REQUEST['photoTime'];  */
+	if (array_key_exists('resolution', $exif)) {
+      $resolution = $exif['resolution'];
+    }else
+	if (array_key_exists('camera', $exif)) {
+      $camera = $exif['camera'];
+    }else
+	if (array_key_exists('aperture', $exif)) {
+      $aperture = $exif['aperture'];
+    }else
+    if (array_key_exists('exposure', $exif)) {
+      $exposure = $exif['exposure'];
+    }else
+	if (array_key_exists('isoSpeed', $exif)) {
+      $isoSpeed = $exif['isoSpeed'];
+    }else	
+	if (array_key_exists('focalLength', $exif)) {
+      $focalLength = $exif['focalLength'];
+    }else
+	if (array_key_exists('saturation', $exif)) {
+      $saturation = $exif['saturation'];
+    }else
+	if (array_key_exists('whiteBalance', $exif)) {
+      $whiteBalance = $exif['whiteBalance'];
+    }else
+	if (array_key_exists('photoTime', $exif)) {
+      $photoTime = $exif['photoTime'];
+    }else	
 	$sql = "update exif set resolution='$resolution',camera='$camera',aperture='$aperture',exposure='$exposure',isoSpeed='$isoSpeed',
 		focalLength='$focalLength',saturation='$saturation',whiteBalance='$whiteBalance',photoTime='$photoTime' where id=$id;";
 		$pdo->exec($sql);
