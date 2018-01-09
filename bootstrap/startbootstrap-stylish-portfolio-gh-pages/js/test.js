b6c6a39323ba330f1b32a347f31c9a433c02e4d8
QQYamBall <s104213019@mail1.ncnu.edu.tw>
2018-01-10 02:28:39 +0800

    快完成版

--- bootstrap/startbootstrap-stylish-portfolio-gh-pages/js/test.js
+++ bootstrap/startbootstrap-stylish-portfolio-gh-pages/js/test.js
@@ -1,34 +1,54 @@
 function list_Photo(){
-  console.log('good');
 		$.ajax({
 		  type: 'GET',
       dataType :'json',
       url: "http://localhost/frog-master/add/show.php?act=frog",
       success : function(data){
         //data[0].filepic = data[0].filepic.replace(/%/g, "+");
-        console.log(data[0].filepic);
         //reader.readAsDataURL(data);
         $('#showtable').html('');
         for(i=0;i<data.length;i=i+1){
-          $('#showtable').prepend(
-            `
-            <a data.id="${i}" herf="#" class="JM"><img width="300px" height="200px" src="data:image/jpeg;base64,${data[i].filepic}"></a>
-            `
-      )}
+          $('#showtable').prepend(`
+            <a data.id=${data[i].id} herf="#" name="${data[i].filename}" class="JM"><img width="300px" height="200px" src="data:image/jpeg;base64,${data[i].filepic}"></a>
+            `)
+        }
     },error : function(err){
       console.log('err');
     },
   });
 }
+function Post_id(name){
+  $.ajax({
+   type: 'POST',
+   url: "http://localhost/frog-master/add/showexif.php",
+   data:{ filename : name  },
+   dataType:'json',
+   success: function(data){
+        $('#modalshow').append(
+        `<div>textName = <input type="text" id="textName" placeholder="${data[0].textName}" ></div><br/>
+        <div>textIntroduce = <input type="text" id="textIntroduce" placeholder="${data[0].textIntroduce}" > </div><br/>
+        <div>resolution = <input type="text" id="resolution" placeholder="${data[0].resolution}" > </div><br/>
+        <div>camera = <input type="text" id="camera" placeholder="${data[0].camera}" > </div><br/>
+        <div>aperture = <input type="text" id="aperture" placeholder="${data[0].aperture}" > </div><br/>
+        <div>exposure = <input type="text" id="exposure" placeholder="${data[0].exposure}" > </div><br/>
+        <div>IsoSpeed = <input type="text" id="IsoSpeed" placeholder="${data[0].isoSpeed}" > </div><br/>
+        <div>focalLength = <input type="text" id="focalLength" placeholder="${data[0].focalLength}" > </div><br/>
+        <div>saturation = <input type="text" id="saturation" placeholder="${data[0].saturation}" > </div><br/>
+        <div>whiteBalance = <input type="text" id="whiteBalance" placeholder="${data[0].whiteBalance}" > </div><br/>
+        <div>photoTime = <input type="text" id="photoTime" placeholder="${data[0].photoTime}" > </div> `
+      );
+    },
+  });
+}
 $('#showtable').on("click",'.JM',function(){
 	var src = $(this).html();
-	$('#modalshow').html(src);
-	console.log("安安");
+	$('#modalshow').append(src);
 	var id = $(this).prop("id");
-  QQ(id);
+  var filename = $(this).prop("name");
+  Post_id(filename);
+  changepic(id,filename);
 });
 $('#frogbutton').on("click",function(){
-    console.log("success!");
     list_Photo();
 });
 function List(){
@@ -45,10 +65,45 @@ $('#frogicon').on("click",function(){
     );
   }
 });
-$('#showtable').on("click",function(){
-  console.log("nice");
-  ;
-});
-function QQ(id){
-	console.log(id);
-}
+function changepic(id,filename){
+  console.log('change');
+    $('#edit').on('click',function(){
+        ajax({
+            url: "http://localhost/frog-master/add/edit.php?act=edit",
+            type:"POST",
+            data:{
+            textName:$('#textName').val(),
+            textIntroduce:$('#textIntroduce').val(),
+            resolution:$('#resolution').val(),
+            camera:$('#camera').val(),
+            aperture:$('#aperture').val(),
+            exposure:$('#exposure').val(),
+            IsoSpeed:$('#IsoSpeed').val(),
+            focalLengh:$('#focalLength').val(),
+            saturation:$('#saturation').val(),
+            whiteBalance:$('#whiteBalance').val(),
+            photoTime:$('#photoTime').val(),
+            id:id,
+            },
+            success:function(data){
+                QQ(data);
+            },
+            error:function(error){
+                alert("QQ");
+            }
+        });
+    });
+    $('delete').on('click',function(){
+        ajax({
+            url: "http://localhost/frog-master/add/edit.php?act=delete",
+            type:"POST",
+            data:{id:id},
+            success:function(data){
+                alert("ok");
+            },
+            error:function(error){
+                alert("QQ");
+            }
+        });
+    });
+};
