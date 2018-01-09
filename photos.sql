8b4aa084dd6391b83fb9e747498cd874d2f91f8a
kukula87 <kyky159k@gmail.com>
2018-01-10 00:36:16 +0800

    更新資料庫

--- photos.sql
+++ photos.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2018-01-08 17:59:12
--- 伺服器版本: 10.1.26-MariaDB
--- PHP 版本： 7.1.9
+-- 產生時間： 2018-01-09 17:31:37
+-- 伺服器版本: 10.1.22-MariaDB
+-- PHP 版本： 7.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `exif` (
   `id` int(11) NOT NULL,
+  `filename` varchar(50) NOT NULL,
   `resolution` char(50) NOT NULL,
   `camera` char(50) NOT NULL,
   `aperture` char(50) NOT NULL,
@@ -49,7 +50,7 @@ CREATE TABLE `exif` (
 
 CREATE TABLE `frogphotos` (
   `id` int(11) NOT NULL,
-  `filepic` blob NOT NULL,
+  `filepic` longblob NOT NULL,
   `filename` varchar(50) NOT NULL,
   `filetype` varchar(50) NOT NULL,
   `filesize` int(10) NOT NULL,
@@ -65,13 +66,15 @@ CREATE TABLE `frogphotos` (
 -- 資料表索引 `exif`
 --
 ALTER TABLE `exif`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `filename` (`filename`);
 
 --
 -- 資料表索引 `frogphotos`
 --
 ALTER TABLE `frogphotos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `filename` (`filename`);
 
 --
 -- 在匯出的資料表使用 AUTO_INCREMENT
@@ -82,13 +85,12 @@ ALTER TABLE `frogphotos`
 --
 ALTER TABLE `exif`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- 使用資料表 AUTO_INCREMENT `frogphotos`
 --
 ALTER TABLE `frogphotos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
+  COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
